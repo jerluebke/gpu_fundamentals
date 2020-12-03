2ef27983e8f1be8fd94376bca5ec2c7840fc3da2
Jeremiah Luebke <jer.luebke@web.de>
2020-12-03 18:19:27 +0100

    fine tuned output

--- HW04_tiled_matmul/main.cu
+++ HW04_tiled_matmul/main.cu
@@ -247,13 +247,15 @@ void check_device_properties(const size_t tile_shape[2])
         / (2*block_size*sizeof(float));
 
     if ( number_of_blocks_by_threads > max_blocks_per_sm )
-        fprintf(stderr, "WARNING: block configuration %zux%zu allows %d blocks, "
-                "but only %d are available on the multiprocessors!\n",
+        fprintf(stderr, "WARNING: blocks too small. block configuration "
+                "%zux%zu allows %d blocks, but only %d are available on the "
+                "multiprocessors!\n",
                 tile_shape[0], tile_shape[1], number_of_blocks_by_threads,
                 max_blocks_per_sm);
     if ( number_of_blocks_by_shared_mem < max_blocks_per_sm )
-        fprintf(stderr, "WARNING: block configuration %zux%zu allows only %d "
-                "blocks, but %d are available on the multiprocessors!\n",
+        fprintf(stderr, "WARNING: blocks require too much shared memory. "
+                "block configuration %zux%zu allows only %d " "blocks, but %d "
+                "are available on the multiprocessors!\n",
                 tile_shape[0], tile_shape[1], number_of_blocks_by_shared_mem,
                 max_blocks_per_sm);
 }
@@ -389,7 +391,8 @@ int perform_matmul_test(
         matrix_t test_matrices_d[4],
         const size_t tile_shape[2],
         timing_result_t timing_result[3],
-        const size_t timing_runs[2])
+        const size_t timing_runs[2],
+        bool do_cpu = true)
 {
     matrix_t hA = test_matrices_h[0];
     matrix_t hB = test_matrices_h[1];
@@ -411,7 +414,9 @@ int perform_matmul_test(
 
 
 #define COMMA ,
-    TIMEIT(timing_runs[0], (&timing_result[0]), mat_mul_cpu(hA COMMA hB COMMA hC);)
+    if ( do_cpu )
+        TIMEIT(timing_runs[0], (&timing_result[0]), mat_mul_cpu(hA COMMA hB COMMA hC);)
+
     TIMEIT(timing_runs[1], (&timing_result[1]),
             mat_mul_global_kernel<<<grid COMMA block>>>(dA COMMA dB COMMA dCg);
             cudaDeviceSynchronize();)
@@ -422,19 +427,21 @@ int perform_matmul_test(
 #undef COMMA
 
 
-    if ( cudaMemcpy(Cref.data, dCg.data, Cref.M*Cref.N*sizeof(float),
-                cudaMemcpyDeviceToHost) != cudaSuccess ) {
-        fprintf(stderr, "cudaMemcpy failed!\n");
-        return -1;
-    }
-    check_results(hC.data, Cref.data, hC.M*hC.N);
+    if ( do_cpu ) {
+        if ( cudaMemcpy(Cref.data, dCg.data, Cref.M*Cref.N*sizeof(float),
+                    cudaMemcpyDeviceToHost) != cudaSuccess ) {
+            fprintf(stderr, "cudaMemcpy failed!\n");
+            return -1;
+        }
+        check_results(hC.data, Cref.data, hC.M*hC.N);
 
-    if ( cudaMemcpy(Cref.data, dCs.data, Cref.M*Cref.N*sizeof(float),
-                cudaMemcpyDeviceToHost) != cudaSuccess ) {
-        fprintf(stderr, "cudaMemcpy failed!\n");
-        return -1;
+        if ( cudaMemcpy(Cref.data, dCs.data, Cref.M*Cref.N*sizeof(float),
+                    cudaMemcpyDeviceToHost) != cudaSuccess ) {
+            fprintf(stderr, "cudaMemcpy failed!\n");
+            return -1;
+        }
+        check_results(hC.data, Cref.data, hC.M*hC.N);
     }
-    check_results(hC.data, Cref.data, hC.M*hC.N);
 
     return 0;
 }
@@ -448,19 +455,20 @@ int perform_matmul_test(
 
 const char* HEADER = "+=========+\n"
                      "| mat_mul |\n"
-                     "+=========+\n";
+                     "+=========+\n"
+                     "C = A * B\n";
 const char* START_COMP = "Performing computations (cpu timing runs: %d, gpu timing runs: %d)...\n";
-const char* CONFIG = "A shape: (%d, %d), B shape: (%d, %d), tile shape: (%d, %d)\n";
+const char* CONFIG = "A shape: (%d, %d), B shape: (%d, %d), tile shape: (%d, %d), do cpu: %s\n";
 const char* END_COMP = "Done.\n\n";
 const char* RESULT_HEADER = "Timing Results:\n"
                             "---------------\n"
-                            "(times in ms, ratios gpu/cpu)\n\n";
+                            "(times in ms, ratios gpu/cpu)\n";
 const char* RESULT_START = "A shape: %d x %d, B shape: %d x %d\n"
-                           "+-------++----------+--------+--------++-------+-------+------+\n"
-                           "| tile  || CPU      | global | shared || g/cpu | s/cpu | s/g  | \n"
-                           "+-------++----------+--------+--------++-------+-------+------+\n";
-const char* RESULT_ROW = "| %2dx%2d || %9.2f | %5.2f  | %5.2f  || %5.3f | %5.3f | %5.3f |\n";
-const char* RESULT_END = "+-------++----------+--------+--------++-------+-------+------+\n";
+                           "+-------++----------+--------+--------++--------+--------+-------+\n"
+                           "| tile  || CPU      | global | shared || g/cpu  | s/cpu  | s/g   | \n"
+                           "+-------++----------+--------+--------++--------+--------+-------+\n";
+const char* RESULT_ROW = "| %2dx%2d || %8.2f | %6.2f | %6.2f || %6.3f | %6.3f | %5.3f |\n";
+const char* RESULT_END = "+-------++----------+--------+--------++--------+--------+-------+\n";
 
 
 
@@ -469,7 +477,7 @@ int main()
     srand(time(NULL));
 
     const int tile_shape_count = 4;
-    const int matrix_shape_count = 3;
+    const int matrix_shape_count = 5;
     const size_t tile_shapes[tile_shape_count][2] = {
         {  8,  8 },
         {  8, 16 },
@@ -481,12 +489,11 @@ int main()
         {  300,  100,  500 },
         {  600,  200, 1000 },
         { 1200,  400, 2000 },
-        /* { 2400,  800, 4000 },
-        { 3000, 1000, 5000 }, */
-        /* { 4800, 1600, 8000 }, */
+        { 2400,  800, 4000 },
+        { 3000, 1000, 5000 },
     };
 
-    const size_t timing_runs[2] = { 0, 1000 };
+    const size_t timing_runs[2] = { 7, 1000 };
     timing_result_t timing_results[3*matrix_shape_count*tile_shape_count];
     matrix_t test_matrices_h[4];
     matrix_t test_matrices_d[4];
@@ -503,12 +510,15 @@ int main()
                 test_matrices_h, test_matrices_d, matrix_shapes[i]);
 
         for ( int j = 0; j < tile_shape_count; ++j ) {
+            bool do_cpu = j == 0 && i < 3;
             printf(CONFIG, test_matrices_h[0].M, test_matrices_h[0].N,
                            test_matrices_h[1].M, test_matrices_h[1].N,
-                           tile_shapes[j][0], tile_shapes[j][1]);
+                           tile_shapes[j][0], tile_shapes[j][1],
+                           do_cpu ? "true" : "false");
 
             perform_matmul_test(test_matrices_h, test_matrices_d, tile_shapes[j],
-                    &timing_results[3*(i*matrix_shape_count+j)], timing_runs);
+                    &timing_results[3*(i*matrix_shape_count+j)], timing_runs,
+                    do_cpu);
         }
         free_test_matrices(test_matrices_h, test_matrices_d);
     }
@@ -517,19 +527,36 @@ int main()
 
     puts(RESULT_HEADER);
     for ( int i = 0; i < matrix_shape_count; ++i ) {
-        printf(RESULT_START, test_matrices_h[0].M, test_matrices_h[0].N,
-                             test_matrices_h[1].M, test_matrices_h[1].N);
+        printf(RESULT_START, matrix_shapes[i][0], matrix_shapes[i][1],
+                             matrix_shapes[i][1], matrix_shapes[i][2]);
+
         for ( int j = 0; j < tile_shape_count; ++j ) {
-            float cpu_time = timing_results[3*(i*matrix_shape_count+j)].t_mean;
+            bool do_cpu = j == 0 && i < 3;
             float global_time = timing_results[3*(i*matrix_shape_count+j)+1].t_mean;
             float shared_time = timing_results[3*(i*matrix_shape_count+j)+2].t_mean;
+            float sg_ratio = shared_time / global_time;
+            float cpu_time, gc_ratio, sc_ratio;
+            if ( do_cpu ) {
+                cpu_time = timing_results[3*(i*matrix_shape_count)].t_mean;
+                gc_ratio = global_time / cpu_time;
+                sc_ratio = shared_time / cpu_time;
+            }
+            else if ( i < 3 ) {
+                cpu_time = timing_results[3*(i*matrix_shape_count)].t_mean;
+                gc_ratio = global_time / cpu_time;
+                sc_ratio = shared_time / cpu_time;
+            }
+            else {
+                cpu_time = -1;
+                gc_ratio = -1;
+                sc_ratio = -1;
+            }
             printf(RESULT_ROW, tile_shapes[j][0], tile_shapes[j][1],
                                cpu_time, global_time, shared_time,
-                               global_time/cpu_time, shared_time/cpu_time,
-                               shared_time/global_time);
+                               gc_ratio, sc_ratio, sg_ratio);
         }
+        puts(RESULT_END);
     }
-    puts(RESULT_END);
 
 
     return EXIT_SUCCESS;
